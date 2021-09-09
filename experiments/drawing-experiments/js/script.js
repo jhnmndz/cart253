bcf15e86764a46465aaf2f2eed8dc08b91a29238
jhnmndz <j_oz@live.concordia.ca>
2021-09-09 09:14:33 -0400

    Drawing Experiment: Added color to shapes

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -23,9 +23,15 @@ Description of setup
 function setup() {
   createCanvas(500,500);
   background(200,200,200);
+  noStroke()
   rectMode(CORNER);
+  fill(127,100,80,100);
   rect(250,250,100,100,5);
+  fill(127,100,60,50);
   rect(250,250,50,50,3);
+  fill(127,100,25,50);
+  rect(250,250,25,25,3);
+
 
 
 }
