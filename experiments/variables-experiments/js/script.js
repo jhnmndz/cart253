03d33887f84d12f20be4fa054398b8d4fcf8bcc9
jhnmndz <j_oz@live.concordia.ca>
2021-09-18 17:58:45 -0400

    Learned random variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -9,10 +9,11 @@ author, and this description to match your project!
 "use strict";
 let bgShade=0
 let circle={
-  x:0,
+  x:250,
   y:250,
-  size:300,
-  speed:2
+  size:100,
+  speed:2,
+  fill:0
 };
 
 /**
@@ -28,8 +29,13 @@ Description of draw()
 */
 function draw() {
   background(bgShade);
-  circle.x+=circle.speed
+  circle.x+=circle.speed;
+  circle.fill=random(0,255);
+  circle.speed=random(-5,5);
+  fill(circle.fill);
   ellipse(circle.x,circle.y,circle.size);
 
-  console.log(`circle ${circle}`)+circle
+  let randomNumber=random();
+
+  console.log(randomNumber);
 }
