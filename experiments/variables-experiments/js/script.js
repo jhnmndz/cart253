468dcb037f8896afcaa8ae6f0082962db30f50ef
jhnmndz <j_oz@live.concordia.ca>
2021-09-18 18:13:02 -0400

    Learned map and constrain

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -9,11 +9,11 @@ author, and this description to match your project!
 "use strict";
 let bgShade=0
 let circle={
-  x:250,
+  x:0,
   y:250,
   size:100,
   speed:2,
-  fill:0
+  fill:255
 };
 
 /**
@@ -30,12 +30,8 @@ Description of draw()
 function draw() {
   background(bgShade);
   circle.x+=circle.speed;
-  circle.fill=random(0,255);
-  circle.speed=random(-5,5);
+  circle.x=constrain(circle.x,0,width);
+  circle.fill=map(circle.x,0,width,0,255);
   fill(circle.fill);
   ellipse(circle.x,circle.y,circle.size);
-
-  let randomNumber=random();
-
-  console.log(randomNumber);
 }
