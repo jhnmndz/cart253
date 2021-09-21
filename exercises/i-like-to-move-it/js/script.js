b36dd44e2e34eebb6939d520d9c9a8e1182b1454
jhnmndz <j_oz@live.concordia.ca>
2021-09-21 17:49:54 -0400

    E1: Experimenting

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,33 +1,43 @@
 /**
-Title of Project
-Author Name
+E1: I like to move it!
+John Mendoza
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Using map() and constrain() draw functions to declare and change variables.
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
+let star1 = {
+  x1: 20,
+  y1: 60,
+  x2: 50,
+  y2: 50,
+  x3: 30,
+  y3: 70,
+  x4: 15,
+  y4: 30,
+  speed: 1,
 
 }
 
 
 /**
-Description of setup
+Draws canvas
 */
 function setup() {
-
+  createCanvas(500,500);
+  background(0);
 }
 
 
 /**
-Description of draw()
+
 */
 function draw() {
-
+  fill(255);
+  push();
+  translate(width * 0.2, height * 0.5);
+  rotate(frameCount / 200.0);
+  star(0, 0, 5, 70, 3);
+  pop();
 }
