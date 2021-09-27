1f8d9b0488be566626283d81b96071e1df5391ec
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-09-27 19:25:50 -0400

    E3: Velocity and acceleration

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -7,15 +7,17 @@ author, and this description to match your project!
 */
 
 "use strict";
-let bg = {
-  r:0,
-  g:0,
-  b:0
-}
+
 let circle = {
   x: 250,
   y: 250,
-  size: 100
+  size: 100,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 5
 }
 /**
 Created a canvas
@@ -29,13 +31,26 @@ function setup() {
 Drawing a loop
 */
 function draw() {
-  background(bg.r,bg.g,bg.b);
+  background(0);
+
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  }
+  else {
+    circle.ax = circle.acceleration;
+  }
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  }
+  else {
+    circle.ay = circle.acceleration;
+  }
+  circle.vx = circle.vx + circle.ax;
+  circle.vx = constrain(circle.vx,-circle.maxSpeed,circle.maxSpeed);
+  circle.vy = circle.vy + circle.ay;
+  circle.vy = constrain(circle.vy,-circle.maxSpeed,circle.maxSpeed);
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
   ellipse(circle.x,circle.y,circle.size);
-}
-
-function mouseDragged() {
-  bg.r = random(0,255);
-  bg.g = random(0,255);
-  bg.b = random(0,255);
-
 }
