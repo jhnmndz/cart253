879315530fb1876bc8071ac4e7ade429dab3e86e
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-09-27 13:43:19 -0400

    E3: Conditionals experiments

---
+++ experiments/conditionals-experiments/js/script.js
@@ -0,0 +1,33 @@
+/**
+Conditionals experiments
+John Mendoza
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+let displayCircle = false;
+
+/**
+Description of setup
+*/
+function setup() {
+createCanvas(500,500);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(0);
+
+  if (mouseIsPressed){
+    displayCircle = true;
+  }
+
+  if (displayCircle) {
+    ellipse(250,250,100,100);
+  }
+}
