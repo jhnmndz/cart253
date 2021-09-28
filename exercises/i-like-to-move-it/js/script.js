c8f8ac38157695082854bcbf3a47b3a08d161f8b
jhnmndz <j_oz@live.concordia.ca>
2021-09-27 22:41:57 -0400

    Update script.js

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -7,37 +7,44 @@ Using map() and constrain() draw functions to declare and change variables.
 
 "use strict";
 
-let star1 = {
-  x1: 20,
-  y1: 60,
-  x2: 50,
-  y2: 50,
-  x3: 30,
-  y3: 70,
-  x4: 15,
-  y4: 30,
-  speed: 1,
-
+let rectangle1 = {
+  x: 0,
+  y: 0,
+  xSize: 100,
+  ySize: 600,
+  speed: 5,
 }
 
+let rectangle2 = {
+  x: 0,
+  y: 0,
+  size: 50,
+  speed: 1,
+}
 
 /**
 Draws canvas
 */
 function setup() {
-  createCanvas(500,500);
-  background(0);
+  createCanvas(600,600);
+
+
 }
 
 
 /**
-
+Draws moving shapes using maps and constrains
 */
 function draw() {
-  fill(255);
-  push();
-  translate(width * 0.2, height * 0.5);
-  rotate(frameCount / 200.0);
-  star(0, 0, 5, 70, 3);
-  pop();
+  background(0);
+  rectangle1.fill = map(rectangle1.x,0,600,0,255);
+  fill(rectangle1.fill);
+  noStroke();
+  rect(rectangle1.x,rectangle1.y,rectangle1.xSize,rectangle1.ySize,rectangle1.speed)
+  rectangle1.x = rectangle1.x + rectangle1.speed;
+  rectangle2.fill = map(rectangle2.x,0,600,0,255);
+  fill(mouseY);
+  stroke(0);
+  rect(mouseX,mouseY,mouseX);
+
 }
