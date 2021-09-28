ee822d6504033878097cce964e24a0bebfa721e9
jhnmndz <j_oz@live.concordia.ca>
2021-09-28 00:33:45 -0400

    E2: Started Dodge em exercise

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+E2: Dodge Em
+John Mendoza
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -9,25 +9,68 @@ author, and this description to match your project!
 "use strict";
 
 
-/**
-Description of preload
-*/
-function preload() {
-
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g:0,
+    b:0
+  }
 }
 
-
+let user = {
+  x: 250,
+  y: 250,
+  size: 100,
+  fill: 255
+}
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
+  covid19.y = random(0,height);
+  covid19.vx = covid19.speed;
 }
 
 
 /**
-Description of draw()
+COVID19 movement
 */
 function draw() {
+  background(0);
+
+
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0,height);
+  }
+
+//User movement
+  user.x = mouseX;
+  user.y = mouseY;
+
+//Touching the virus
+  let d = dist(user.x,user.y,covid19.x,covid19.y);
+  if (d < covid19.size/2 + user.size/2) {
+    noLoop();
+  }
+
+
+//Draw COVID19 circle
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  ellipse(covid19.x,covid19.y,covid19.size);
+
+//Draw User
+  fill(user.fill);
+  ellipse(user.x,user.y,user.size);
 
 }
