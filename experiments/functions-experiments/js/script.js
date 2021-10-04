894d907288cae6e7465489b4a9bf18430b785941
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 10:23:16 -0400

    E4: Started text tutorials

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,38 +1,25 @@
 /**
-E4: Functions with return values
+E4: Functions using text
 John Mendoza
 
-Calculation of a situation (return values)
+Displays text
 */
 
 "use strict";
 
 /**
-Draws a
+Draws canvas
 */
 function setup() {
   createCanvas(500,500);
 
-  let hotCelsius = toCelsius(100);
-  console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`)
-
-  let coldCelsius = toCelsius(10);
-  console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`)
 }
 
 
 /**
-Draws lines
+Draws text
 */
 function draw() {
   background(0);
 
-}
-
-/**
-Called "Function signature"
-*/
-function toCelsius(fahrenheit){
-  let celsius = (fahrenheit - 32) * 5/9;
-  return celsius;
 }
