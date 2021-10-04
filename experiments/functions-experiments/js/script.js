a5f180e28eea4ba348c79e5610dbb460c702fd61
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 08:55:20 -0400

    E4: Started functions experiments

---
+++ experiments/functions-experiments/js/script.js
@@ -0,0 +1,32 @@
+/**
+Functions experiment 1
+John Mendoza
+
+Following the youtube playlist for functions.
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
