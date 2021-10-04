f0ac5f1e3f1f3609462bf0624b07160be45ee27e
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 16:58:02 -0400

    A5: Started looking for love

---
+++ activities/05-looking-for-love/js/script.js
@@ -0,0 +1,32 @@
+/**
+A5: Looking for love
+John Mendoza
+
+Using functions to find love
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
