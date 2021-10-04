a510e9831e7846a24889d10fc997d01d75b3b813
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 16:14:54 -0400

    E4: Added keyCodes

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -23,4 +23,19 @@ Draws text animation
 function draw() {
   background(bg);
 
+  textAlign(CENTER,CENTER);
+  textSize(64);
+  fill(255);
+  text(keyCode,width/2,height/2);
+}
+
+function keyPressed(){
+  if (keyCode === UP_ARROW){
+    bg = bg + 10;
+    bg = constrain(bg,0,255);
+  }
+  else if (keyCode === DOWN_ARROW){
+    bg = bg - 10;
+    bg = constrain(bg,0,255)
+  }
 }
