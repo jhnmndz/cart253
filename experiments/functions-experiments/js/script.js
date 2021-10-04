59a623cc8f523c54c628d738792e26f82dcca809
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 09:57:44 -0400

    E4: Specified parameters with functions

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -6,7 +6,6 @@ John Mendoza
 
 "use strict";
 
-
 /**
 Draws a
 */
@@ -16,8 +15,27 @@ function setup() {
 
 
 /**
-Draws a
+Draws lines
 */
 function draw() {
   background(0);
+
+  /**
+  Function call with parameters
+  */
+  parallels(100,100,5,1,100,3);
+  parallels(50,50,10,2,20,6);
+}
+
+/**
+Defined functions and added parameters(variables)
+*/
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpacing){
+  for (let i = 0; i < numLines; i++){
+    noStroke();
+    fill(255);
+    rectMode(CENTER);
+    rect(x,y,lineWidth,lineHeight);
+    x = x + lineSpacing;
+  }
 }
