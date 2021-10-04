121b3beed2b350793688cb293c91d5232088f3a5
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 10:32:35 -0400

    E4: Represented text

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -3,9 +3,21 @@ E4: Functions using text
 John Mendoza
 
 Displays text
+String of text using "double quotation marks" or 'single quotes' or `back ticks`
+
+Options!
+let string1 = "She said, \"I do not agree!\"";
+let string2 = 'She said, "I don\'t agree!"';
+let string3 = `She said, "I don't agree!"`;
 */
 
 "use strict";
+
+let name = "Hamlet";
+let title = "Prince";
+let country = "Denmark";
+let string = `Hi, my name is ${name}, ${title} of ${country}!`
+let string2 = "Hi, my name is " + name + ", title + " of " + country + "!";
 
 /**
 Draws canvas
