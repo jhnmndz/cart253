3521e5e70ea4c1d5124ee28198ddb252cfa5fb9e
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 09:06:10 -0400

    E4: Defined variables and added velocity

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -3,30 +3,35 @@ Functions experiment 1
 John Mendoza
 
 Following the youtube playlist for functions.
+Typing function to define it, then call for it.
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
+let circle={
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0,
 
 }
 
-
 /**
-Description of setup
+Defined as a function definition
 */
 function setup() {
-
+  createCanvas(500,500);
 }
 
 
 /**
-Description of draw()
+Draws a moving circle
 */
 function draw() {
-
+  background(0);
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+  fill(255,0,0);
+  ellipse(circle.x,circle.y,circle.size);
 }
