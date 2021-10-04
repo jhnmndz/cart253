cfb66f1fffc7e034165ce4028b0ff5fe20a4b558
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 10:21:18 -0400

    E4: Completed return values

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -2,6 +2,7 @@
 E4: Functions with return values
 John Mendoza
 
+Calculation of a situation (return values)
 */
 
 "use strict";
@@ -11,6 +12,12 @@ Draws a
 */
 function setup() {
   createCanvas(500,500);
+
+  let hotCelsius = toCelsius(100);
+  console.log(`100 degrees Fahrenheit is ${hotCelsius} degrees Celsius.`)
+
+  let coldCelsius = toCelsius(10);
+  console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`)
 }
 
 
@@ -20,8 +27,12 @@ Draws lines
 function draw() {
   background(0);
 
-  let x = random(0,width);
-  let y = random(0,height);
+}
 
-  ellipse(x,y,100);
+/**
+Called "Function signature"
+*/
+function toCelsius(fahrenheit){
+  let celsius = (fahrenheit - 32) * 5/9;
+  return celsius;
 }
