cb8b69a5779d3ca414cf896700f3434acd4c073e
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 15:57:08 -0400

    E4: Completed states tutorial
    - added conditionals
    - added functions
    - used states to reduce clutter

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -16,24 +16,61 @@ let circle = {
   speed: 2
 }
 
+let state = `title`; //Possible states are: title, animation, ending
+
 /**
 Draws canvas
 */
 function setup() {
   createCanvas(500,500);
   circle.vx = circle.speed;
+  textSize(32);
+  textAlign(CENTER,CENTER);
 }
 
 
 /**
-Draws text
+Draws text animation
 */
 function draw() {
   background(0);
 
+  if (state === `title`){
+    title();
+    }
+  else if (state === `animation`){
+    animation();
+    }
+  else if (state === `ending`){
+    ending();
+    }
+}
+
+function title(){
+  //Title
+  fill(255)
+  text(`Life.`,width/2,height/2);
+}
+
+function animation(){
+  //Animation
   circle.x = circle.x + circle.vx;
   circle.y = circle.y + circle.vy;
 
+  if (circle.x > width) {
+    state = `ending`;
+  }
   ellipse(circle.x,circle.y,circle.size);
+}
 
+function ending(){
+  //Ending
+  fill(127);
+  text(`It's all over`,width/2,height/2)
+}
+
+function keyPressed() {
+  if (state === `title`){
+    state = `animation`;
+  }
 }
