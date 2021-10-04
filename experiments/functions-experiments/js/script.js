5d2d75383e77df589d3ac0c9b02bcbf8151845ee
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 16:01:34 -0400

    E4: Started keyboard input

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,76 +1,26 @@
 /**
-E4: Functions using states
+E4: Functions using keyboard inputs
 John Mendoza
 
-Using state to organize
+Learning how to use keyboard inputs
 */
 
+let bg = 0;
+
 "use strict";
-
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 0,
-  vy: 0,
-  speed: 2
-}
-
-let state = `title`; //Possible states are: title, animation, ending
 
 /**
 Draws canvas
 */
 function setup() {
   createCanvas(500,500);
-  circle.vx = circle.speed;
-  textSize(32);
-  textAlign(CENTER,CENTER);
-}
 
+}
 
 /**
 Draws text animation
 */
 function draw() {
-  background(0);
+  background(bg);
 
-  if (state === `title`){
-    title();
-    }
-  else if (state === `animation`){
-    animation();
-    }
-  else if (state === `ending`){
-    ending();
-    }
-}
-
-function title(){
-  //Title
-  fill(255)
-  text(`Life.`,width/2,height/2);
-}
-
-function animation(){
-  //Animation
-  circle.x = circle.x + circle.vx;
-  circle.y = circle.y + circle.vy;
-
-  if (circle.x > width) {
-    state = `ending`;
-  }
-  ellipse(circle.x,circle.y,circle.size);
-}
-
-function ending(){
-  //Ending
-  fill(127);
-  text(`It's all over`,width/2,height/2)
-}
-
-function keyPressed() {
-  if (state === `title`){
-    state = `animation`;
-  }
 }
