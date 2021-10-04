1952583e890e85b0d4c4d9421a5466594ac566fa
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-04 17:37:39 -0400

    A5: Created a plan

--- activities/05-looking-for-love/js/script.js
+++ activities/05-looking-for-love/js/script.js
@@ -3,10 +3,29 @@ A5: Looking for love
 John Mendoza
 
 Using functions to find love
+
+Plan:
+1. Set up the program
+  Create objects representing our circles looking for love
+  Do basic set up of our canvas
+  Assign the circles random starting velocities in setup
+  Set the background in draw
+2. Work on the simulation part
+  Get the circles moving and display them
+  Check for either circle moving off the screen (sadness)
+  Check for the circles intersecting (love)
+  Move the simulation stuff into functions
+3. Add the idea of states (title, simulation, love, sadness)
+  Create a state variable and check it in draw
+  Add title, love, and sadness functions that use simple text
+  Connect the states (click to begin, simulation leads to outcomes)
 */
 
 "use strict";
 
+let circle1 = {
+  x:
+}
 
 /**
 Description of preload
@@ -20,7 +39,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(341,143)
 }
 
 
