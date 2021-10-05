4015208fb3cfbaa96037f6dea1a4c5dafbc55be1
jhnmndz <j_oz@live.concordia.ca>
2021-10-05 10:07:09 -0400

    E3: Started Love, Actually exercise
    Made a plan

---
+++ exercises/love-actually/js/script.js
@@ -0,0 +1,39 @@
+/**
+E3: Love, Actually
+John Mendoza
+
+Using functions to find love
+
+Plan:
+1. Set up the program
+  Create objects looking for love
+  Do basic set up of our canvas
+  Assign the objects random starting velocities in setup
+  Set the background in draw
+2. Work on the simulation part
+  Get the objects moving and display them
+  Check for either object moving off the screen
+  Check for the circles intersecting
+  Move the simulation stuff into functions
+3. Add the idea of states
+  Create a state variable and check it in draw
+  Connect the states
+*/
+
+"use strict";
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
