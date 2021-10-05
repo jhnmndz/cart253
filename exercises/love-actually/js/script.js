da58ea378bb182ba36102109094cf38aab245e21
jhnmndz <j_oz@live.concordia.ca>
2021-10-05 11:06:41 -0400

    E3: Movement for circle1
    - Create variable for main circle
    - Added keyboard inputs for controlled movement

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -6,7 +6,7 @@ Using functions to find love
 
 Plan:
 1. Set up the program
-  Create objects looking for love
+  Create objects looking for love in a crowd
   Do basic set up of our canvas
   Assign the objects random starting velocities in setup
   Set the background in draw
@@ -23,17 +23,50 @@ Plan:
 "use strict";
 
 
+let circle1 = {
+  x: 100,
+  y: 100,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+}
+
 /**
 Description of setup
 */
 function setup() {
-
+  createCanvas(500,500);
 }
 
 
 /**
-Description of draw()
+Draws circles()
 */
 function draw() {
-
+  background(247, 54, 177);
+  //Display circles
+  ellipse(circle1.x,circle1.y,circle1.size)
+  //Movement Controls
+  if (keyIsDown(LEFT_ARROW)){
+    circle1.vx = -circle1.speed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    circle1.vx = circle1.speed;
+  }
+  else {
+    circle1.vx = 0;
+  }
+  if (keyIsDown(UP_ARROW)){
+    circle1.vy = -circle1.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    circle1.vy = circle1.speed;
+  }
+  else {
+    circle1.vy = 0;
+  }
+  //Circle movements
+  circle1.x = circle1.x + circle1.vx;
+  circle1.y = circle1.y + circle1.vy;
 }
