f3bc8ba9773f4c0d8cadbb2ee4e5d09fee764472
jhnmndz <90224199+jhnmndz@users.noreply.github.com>
2021-10-15 14:07:14 -0400

    P1: Factored (some) functions

--- project1/js/script.js
+++ project1/js/script.js
@@ -22,11 +22,11 @@ Plan:
   Connect the states
 
 Concept:
-1. ball will avoid movingBars moving towards
-2. movingBars will randomly move on the x-axis
+1. ball will avoid bars moving towards
+2. bars will randomly move on the x-axis
 3. background will indicate build up of adrenaline
-4. once background filled, bars will be replaced with movingBalls
-5. movingBalls are indicating adrenaline
+4. once background filled, bars will be replaced with bubbles
+5. bubbles are indicating adrenaline
 6. ball will begin to shake with energy until optimal productivity levels are reached.
 7. You win, you're productive... until you aren't.
 */
@@ -47,11 +47,11 @@ let ball = { //showcasing avoidance behaviour until the very last second
   b: 255,
 }
 
-let movingBar = { //indicated as intimidating deadline (personal and work)
-  x: undefined,
+let bar = { //indicated as intimidating deadline (personal and work)
+  x: 0,
   y: 0,
-  xMovingBar: 300,
-  yMovingBar: 10,
+  xBar: undefined,
+  yBar: 50,
   vx: 0,
   vy: 0,
   r: 200,
@@ -59,11 +59,11 @@ let movingBar = { //indicated as intimidating deadline (personal and work)
   b: 200,
 }
 
-let movingBall = { //indicated as productivity bubbles
+let bubble = { //indicated as productivity bubbles
   x: undefined,
   y: 0,
-  xMovingBall: 300,
-  yMovingBall: 10,
+  xBubble: 300,
+  yBubble: 10,
   vx: 0,
   vy: 0,
   r: 200,
@@ -79,6 +79,9 @@ let workspace = { //minimal space
   b: 0
 }
 
+let timer = 0; //timer until productivity state
+
+let state = `title`; //instructions on how to reach optimum productivity
 
 
 /**
@@ -101,16 +104,48 @@ Displaying ball with its many responsibilities
 */
 function draw() {
   background(workspace.r, workspace.g, workspace.b);
+
+  // if (state === `title`) {
+  //   title();
+  // }
+  // else if (state === `simulation`) {
+  //   simulation();
+  // }
+  // else if (state === `productive`) {
+  //   productive();
+  // }
+  // else if (state === `stressed`) {
+  //   stressed();
+  // }
+
+  display();
+  ballMovement();
+
+  //Officially stressed (losing state)
+  // let d = dist(ball.x,ball.y,bar.x,bar.y);
+  // if (d < ball.size + bar.size)
+}
+
+function display() {
+  displayBall();
+  displayBar();
+  displayBubble();
+}
+
+function displayBall() {
   //display of the ball
   push();
   fill(ball.r, ball.g, ball.b);
   noStroke();
   ellipse(ball.x, ball.y, ball.xBall, ball.yBall);
   pop();
+}
+
+function ballMovement() {
   //ball movement
   ball.x = ball.x + ball.vx;
-  ball.y = ball.y + ball.vy;
-  //Controlled movements (keyboard inputs)
+  ball.x = constrain(ball.x,0,width);
+  //Controlled movements of ball (left and right keys only)
   if (keyIsDown(LEFT_ARROW)){
     ball.vx = -ball.speed;
   }
@@ -120,19 +155,22 @@ function draw() {
   else {
     ball.vx = 0;
   }
-  //display of the movingBar
+}
+
+function displayBar() {
+  //display of the bar
   push();
-  fill(movingBar.r, movingBar.g, movingBar.b);
+  fill(bar.r, bar.g, bar.b);
   noStroke();
-  ellipse(movingBar.x, movingBar.y, movingBar.xMovingBar, movingBar.yMovingBar);
+  rect(bar.x, bar.y, bar.xBar, bar.yBar);
   pop();
-  //display of the movingBall
+}
+
+function displayBubble() {
+  //display of the bubble
   push();
-  fill(movingBall.r, movingBall.g, movingBall.b);
+  fill(bubble.r, bubble.g, bubble.b);
   noStroke();
-  ellipse(movingBall.x, movingBall.y, movingBall.xMovingBall, movingBall.yMovingBall);
+  ellipse(bubble.x, bubble.y, bubble.xBubble, bubble.yBubble);
   pop();
-  //Officially stressed (losing state)
-  // let d = dist(ball.x,ball.y,movingBar.x,movingBar.y);
-  // if (d < ball.size + movingBar.size)
 }
